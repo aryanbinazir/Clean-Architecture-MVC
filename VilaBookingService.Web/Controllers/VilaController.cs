e9553eb19358954161ca6b52177e1699418c9731
Aryan Binazir <binazir.kobe@gmail.com>
2026-02-20 17:42:56 +0330

    Prepare crud operations and UI for villa entity

--- VilaBookingService.Web/Controllers/VilaController.cs
+++ VilaBookingService.Web/Controllers/VilaController.cs
@@ -31,10 +31,61 @@ namespace VilaBookingService.Web.Controllers
             {
                 await _context.Vilas.AddAsync(obj);
                 await _context.SaveChangesAsync();
+                TempData["success"] = "The villa has been created successfully.";
                 return RedirectToAction("Index");
             }
 
             return View();
         }
+
+        public async Task<IActionResult> Update(int vilaId)
+        {
+            var vila = await _context.Vilas.FindAsync(vilaId);
+            if (vila == null) 
+            {
+                TempData["error"] = $"The villa by this id:{{{vilaId}}} has not found";
+                return RedirectToAction("Error", "Home");
+            }
+            return View(vila);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Vila obj)
+        {
+            if (ModelState.IsValid && obj.Id > 0)
+            {
+                _context.Vilas.Update(obj);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "The villa has been updated successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int vilaId)
+        {
+            var vila = await _context.Vilas.FindAsync(vilaId);
+            if (vila == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(vila);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Vila obj) 
+        {
+            var vila = await _context.Vilas.FindAsync(obj.Id);
+            if (vila == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            _context.Vilas.Remove(vila);
+            await _context.SaveChangesAsync();
+            TempData["success"] = "The villa has been deleted successfully.";
+            return RedirectToAction("Index");
+        }
+
     }
 }
